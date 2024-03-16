cf3cfb943bf264efda8de879953c7a970c2968b7
Janek <m6mmikiisu@gmail.com>
2024-03-16 16:02:53 +0200

    Testing and TDD test century.py functions

--- TestingAndTDD/Class 2/Tasks_extra/Mumbling/fns.py
+++ TestingAndTDD/Class 2/Tasks_extra/Mumbling/fns.py
@@ -1,5 +1,8 @@
 def mumbling(text):
+    # Create new empty string
     new_text = ''
+    # Iterate through given characters and give each letter index
     for index, i in enumerate(text):
+        # For each letter multiply it with index+1 and make a first letter capitalized
         new_text = new_text + ((index + 1) * i).capitalize() + '-'
     return new_text[:-1]
--- TestingAndTDD/Class 2/Tasks_extra/word_mid_char/fns.py
+++ TestingAndTDD/Class 2/Tasks_extra/word_mid_char/fns.py
@@ -1,2 +1,4 @@
 def get_middle(text):
+    # (len(text) - 1) // 2 --> Get start position
+    # (len(text) + 2) // 2 --> Get end position
     return text[(len(text) - 1) // 2:(len(text) + 2) // 2]

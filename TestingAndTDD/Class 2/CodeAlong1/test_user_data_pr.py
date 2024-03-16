43c1702d4e3fe76fbb40f3e2ebc9a2b1e7977b9d
Janek <m6mmikiisu@gmail.com>
2024-03-16 15:14:41 +0200

    Testing and TDD test century.py functions

--- TestingAndTDD/Class 2/CodeAlong1/test_user_data_pr.py
+++ TestingAndTDD/Class 2/CodeAlong1/test_user_data_pr.py
@@ -2,12 +2,13 @@ import user_data_pr
 
 
 def test_read_data(mock_file, test_list_42):
+    """ Test if data from csv file is in the correct format """
     with open(mock_file, 'r') as f:
         prepared_data = user_data_pr.prepare_data(f)
-
     assert prepared_data == test_list_42
 
 
 def test_filter_underage(test_list_42, test_list_13):
+    """ Test if function filters out data where age is under 18 """
     assert user_data_pr.filter_underage(test_list_42) == test_list_42
     assert user_data_pr.filter_underage(test_list_13) == []
--- TestingAndTDD/Class 2/CodeAlong1/user_data_pr.py
+++ TestingAndTDD/Class 2/CodeAlong1/user_data_pr.py
@@ -1,20 +1,30 @@
 def prepare_data(file):
+    # Read the first line from file as headers and make a list from these. Remove empty spaces and split where ',' is.
     headers = file.readline().strip().split(',')
+    # Read the rest of the files as data
     content = file.readlines()
+    # New empty list.
     result_list = []
-
+    # Iterate through data
     for line in content:
+        # For each line in content, create a list
         record = line.strip().split(',')
+        # New empty directory for every value in content
         content_with_headers = {}
+        # Iterate through values in a list
         for index, value in enumerate(record):
+            # Add key: value pair in a new empty dictionary
             content_with_headers[headers[index]] = value
+        # Append newly created dictionary to a directory list
         result_list.append(content_with_headers)
 
     return result_list
 
 
 def filter_underage(people_list):
+    # Iterate through a directory list
     for index, person in enumerate(people_list):
+        # Check if age value in directory is under 18 years. If it is then remove it from a list
         if int(person['age']) < 18:
             people_list.pop(index)
 
@@ -22,5 +32,7 @@ def filter_underage(people_list):
 
 
 with open('real.csv', 'r') as f:
-    list = prepare_data(f)
-    print(filter_underage(list))
+    new_list = prepare_data(f)
+    filter_underage(new_list)
+    print(new_list)
+
